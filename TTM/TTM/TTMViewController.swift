ea3a938098a29b43472d904d8719f261e5a489b9
Ganesh <Ganesh.Budharaju@cdk.com>
2017-08-04 18:53:05 +0530

    added keyboard and actions and cells re aligned

--- TTM/TTM/TTMViewController.swift
+++ TTM/TTM/TTMViewController.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import Foundation
 
-class TTMViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+class TTMViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextViewDelegate
 {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var inputContainerView: UIView!
+    @IBOutlet weak var icvBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ttmTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.tableView.contentInset = UIEdgeInsetsMake(-60, 0, 0, 0)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(TTMViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TTMViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(TTMViewController.hideKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    func hideKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        
+        let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as! CGRect).size
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.3) { 
+            self.icvBottomConstraint.constant = keyboardSize.height
+            self.tableView.setContentOffset(CGPoint(x: 0, y: self.tableView.contentSize.height - 280), animated: true)
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.3) {
+            self.icvBottomConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,7 +69,7 @@ class TTMViewController: UIViewController, UITableViewDataSource, UITableViewDel
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return 3
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat
