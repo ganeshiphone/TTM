cb228817e6722c64133f6d1c49910c601830c0a5
Ganesh <Ganesh.Budharaju@cdk.com>
2017-08-04 14:30:48 +0530

    Added table files and removed un necessary files

---
+++ TTM/TTM/TTMMessageCell.swift
@@ -0,0 +1,26 @@
+//
+//  TTMMessageCell.swift
+//  TTM
+//
+//  Created by Budharaju, Ganesh on 04/08/17.
+//  Copyright © 2017 Budharaju, Ganesh. All rights reserved.
+//
+
+import UIKit
+
+class TTMMessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
